ada163cebffd2172bb59d52f237318776d7216cc
victor <31532226+victorren7@users.noreply.github.com>
2023-04-07 12:02:26 -0700

    connected to DB

--- db/connect.js
+++ db/connect.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
     useUnifiedTopology: true,
   })
 }
