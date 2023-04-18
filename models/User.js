feb6f8fb7c6a9cdbfd4780c3e98bf7dab541bdab
victor <31532226+victorren7@users.noreply.github.com>
2023-04-18 14:45:09 -0700

    updated the error handler

--- models/User.js
+++ models/User.js
@@ -5,19 +5,19 @@ const jwt = require('jsonwebtoken')
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required:[true, 'Please provide a name'],
+    required:[true, 'Please provide name'],
     minLength: 3,
     maxLength: 50,
   },
   email: {
     type: String,
-    required:[true, 'Please provide a email'],
+    required:[true, 'Please provide email'],
     match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'  ],
     unique: true
   },
   password: {
     type: String,
-    required:[true, 'Please provide a name'],
+    required:[true, 'Please provide password'],
     minLength: 6,
   },
 })
